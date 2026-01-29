da4d692a5a561c918222a8781a4921102a6d52e4
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-29 23:53:25 +0530

    array basic structure creat and practice

---
+++ javaScript/1.array-basic.js
@@ -0,0 +1,16 @@
+/*******************************
+ * array => array = [0,1,]; 
+ ********************************/
+
+let marks = [87, 49, 97, 74, 90];
+console.log(marks);
+console.log(marks.length); //property  ( property holo se kichu value dei);
+
+
+let city = ["delhi", "kolkata", "mumbai", "Pune", "chennai", "rajasthan"];
+
+console.log(city);
+console.log(city.length);
+console.log(typeof city); //object
+
+
