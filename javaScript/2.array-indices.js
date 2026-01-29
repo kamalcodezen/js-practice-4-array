b86813fb289d043ce32d689b4dcebb45ef8b581a
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-30 00:35:20 +0530

    array indices | index position search practice

---
+++ javaScript/2.array-indices.js
@@ -0,0 +1,21 @@
+/***********************************
+ * array  indices => array[0] = index
+ *************************************/
+
+
+let marks = [98, 70, 60, 48, 80];
+console.log(marks);
+console.log(marks[3]);//index 3no ta dao amake  | position access;
+
+
+// index diye value change
+marks[2] = 99;  // value change 2nd position ;
+console.log(marks); //new array return
+
+
+
+/*=================================
+1.array value change kora jai => karon javaScript array holo (mutable);
+
+2.string kokhono change hoi na karon string javaScript a holo (immutable); 
+=================================== */
