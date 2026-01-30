5e38e73178be4f00cb89038455fad379e169e837
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-30 19:46:57 +0530

    array method practice push , pop, shift, unshift,shift

---
+++ javaScript/14.isArray.js
@@ -0,0 +1,13 @@
+
+// Array.isArray();  array check 
+
+let cities = ["mumbai", "pune", "delhi", "kerala", "kolkata", "hyderabad"];
+
+
+let food = " roso g o l l a";
+
+console.log(Array.isArray(cities));
+
+console.log(Array.isArray(food));
+
+
---
+++ javaScript/15.all-array-revise.js
@@ -0,0 +1,19 @@
+
+let marks = [78, 90, 78, 96, 92]; //array create
+console.log(marks);
+
+marks.push(999, 888); // push new value add
+console.log(marks);
+
+marks.pop();   //pop last value cutout 
+console.log(marks);
+
+marks.unshift(777, 333); // unshift method fast index|position value add
+console.log(marks);
+
+marks.shift(); // shift method fast position |index value cutout
+console.log(marks);
+
+
+
+
