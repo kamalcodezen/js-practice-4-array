d2cce3a975209955ddfb9486dd603651473fe63f
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-30 16:25:59 +0530

    array pop method practice

---
+++ javaScript/10.pop.js
@@ -0,0 +1,12 @@
+
+let cities = ["mumbai", "pune", "delhi", "kerala", "kolkata", "hyderabad"];
+
+console.log(cities);
+
+let cutEnd1 = cities.pop();
+
+let cutEnd2 = cities.pop();
+
+console.log(cities);
+
+console.log(cutEnd1, cutEnd2);
