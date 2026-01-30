33b97eebe6aafef3c21e36af2a0ba7a154371c32
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-30 15:50:14 +0530

    array index acces practice

---
+++ javaScript/8.index.js
@@ -0,0 +1,19 @@
+
+/**==================================
+ * array index |position access and change .
+ ====================================*/
+
+const number = [3, 4, 8, 56, 23, 24];
+console.log(number);
+
+// array index access 3rd position 
+console.log(number[2]);
+
+let fourthNumber = number[3];
+console.log(fourthNumber);
+
+
+// set or update element value by index
+
+number[2] = 999;
+console.log(number);
