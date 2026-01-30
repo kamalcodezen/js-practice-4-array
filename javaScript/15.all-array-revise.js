0e4ea697685ac0c4e8f7d85776a9902de38e1346
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-30 19:58:42 +0530

    array method practice slice, splice,concat method practice

--- javaScript/15.all-array-revise.js
+++ javaScript/15.all-array-revise.js
@@ -1,5 +1,6 @@
 
-let marks = [78, 90, 78, 96, 92]; //array create
+let marks = [78, 90, 78, 56, 92, 70, 71, 63, 79, 96, 92]; //array create
+let city = ["kolkata", "mumbai", "delhi", "pune", "up"];
 console.log(marks);
 
 marks.push(999, 888); // push new value add
@@ -15,5 +16,17 @@ marks.shift(); // shift method fast position |index value cutout
 console.log(marks);
 
 
+let marksSlice = marks.slice(4, 6); //slice method 
+console.log(marksSlice);
+
+console.log(marks);
 
 
+let marksSplice = marks.splice(4, 2, 988, 899); //splice method (start,delete,add)
+console.log(marksSplice);
+
+console.log(marks);
+
+
+let arrayAdd = marks.concat(city); // more array concat add++
+console.log(arrayAdd);
