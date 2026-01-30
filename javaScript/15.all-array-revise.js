7086a5648bfebbc1086c1370abafa278cee8e958
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-30 20:20:01 +0530

    practice method complete

--- javaScript/15.all-array-revise.js
+++ javaScript/15.all-array-revise.js
@@ -1,6 +1,10 @@
 
 let marks = [78, 90, 78, 56, 92, 70, 71, 63, 79, 96, 92]; //array create
+
 let city = ["kolkata", "mumbai", "delhi", "pune", "up"];
+
+let age = 68;
+
 console.log(marks);
 
 marks.push(999, 888); // push new value add
@@ -30,3 +34,31 @@ console.log(marks);
 
 let arrayAdd = marks.concat(city); // more array concat add++
 console.log(arrayAdd);
+
+
+console.log(city[3]); // array value access 1way indices(index);
+
+
+let checkIncludes = city.includes("mumbai"); //  include method array value check
+console.log(checkIncludes);
+
+if (city.includes("delhi")) {   // include method check another way
+    console.log("This delhi city is available");
+} else {
+    console.log("This  delhi city is not available");
+}
+
+
+
+let checkIndex = city.indexOf("mumbai"); // indexOf method check array value index access
+console.log(checkIndex);
+
+if (city.indexOf("pune")) {  // indexOf method check array value index access another way
+    console.log("This position is true");
+} else {
+    console.log("This position is not true");
+}
+
+
+let checkArray = Array.isArray(age); // Array.isArray method check 
+console.log(checkArray);
