a0d46622296c4358a5c567e22310f0cba6d248ba
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-30 14:25:47 +0530

    array practice array syntax practice

---
+++ javaScript/6.array-basic-2.js
@@ -0,0 +1,12 @@
+
+/**===================================
+ * array -> collection of list same item 
+ ====================================*/
+
+const number = [2, 4, 6, 8, 8];
+
+const cities = ["pune", "mumbai", "delhi", "kolkata"];
+
+const isPassed = [true, false, true, false];
+
+// array syntax ====================
