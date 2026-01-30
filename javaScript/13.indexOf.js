e1d62c4e8dd583ce68dd4e300fdfffc7e60410c4
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-30 17:28:25 +0530

    array index check array.indexOf

---
+++ javaScript/13.indexOf.js
@@ -0,0 +1,9 @@
+
+// array.indexOf() => method index |position search
+
+let companies = ["accenture", "tcs", "wipro", "exl", "genpact"];
+
+
+console.log(companies.indexOf("wipro"));
+
+console.log(companies.indexOf("bmw"));
